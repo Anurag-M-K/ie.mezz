3280828ea9b672cb512843638ca7ad7851aa6441
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-01-14 11:37:17 +0530

    updated text size

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -6,27 +6,27 @@ function HowItWorks() {
     return (
         <div id='work' className='p-10 my-20'>
             <h1 className='text-center text-3xl font-bold my-3'>How we works</h1>
-            <p className='text-center mt-5 mb-10 text-sm  text-gray-600'>We prioritize product excellence, ensuring unwavering commitment to quality and safety, with a dedicated focus on maintaining Halal standards, putting control firmly in your hands</p>
+            <p className='text-center mt-5 mb-10 text-md  text-gray-600'>We prioritize product excellence, ensuring unwavering commitment to quality and safety, with a dedicated focus on maintaining Halal standards, putting control firmly in your hands</p>
             <div className='grid sm:grid-cols-3'>
 
                 <div className='flex px-10 flex-col justify-center items-center'>
 
                     <FaWhatsapp className="text-green-600 w-48 h-28" />
                     <h2 className='text-2xl font-sans font-bold'>Easy to order</h2>
-                    <p className='text-gray-600 text-sm text-center'>Ordering your favorite meal is a breeze with just a few simple steps.</p>
+                    <p className='text-gray-600 text-md text-center'>Ordering your favorite meal is a breeze with just a few simple steps.</p>
                 </div>
                 <div className='flex px-10 flex-col justify-center items-center'>
 
                     <img src='images/icon2.png' className="text-green-600 w-28 h-28" />
                     <h2 className='text-2xl font-sans font-bold'>Fast Delivery</h2>
-                    <p className=' text-gray-600 text-sm text-center'>Experience prompt and efficient delivery services, always ensuring timely arrivals.</p>
+                    <p className=' text-gray-600 text-md text-center'>Experience prompt and efficient delivery services, always ensuring timely arrivals.</p>
                 </div>
                 <div className='flex flex-col justify-center items-center'>
 
                     <MdGppGood
                         className="text-green-400 w-48 h-28" />
                     <h2 className='text-2xl font-sans font-bold'>High quality</h2>
-                    <p className='px-10 text-gray-600 text-sm text-center'>Beyond speed, we prioritize quality, ensuring that every delivery maintains the highest standards of excellence.</p>
+                    <p className='px-10 text-gray-600 text-md text-center'>Beyond speed, we prioritize quality, ensuring that every delivery maintains the highest standards of excellence.</p>
                 </div>
 
             </div>
--- src/components/WhyChoseUs.tsx
+++ src/components/WhyChoseUs.tsx
@@ -14,20 +14,20 @@ function WhyChoseUs() {
       <FaPersonCircleCheck className="text-red-500  w-[100px] h-[100px]" />
         {/* <img src="images/whychoseus1.jpg" className='w-[150] h-[100px]' alt="img" /> */}
         <h3 className='font-bold text-xl mb-3 '>Personalised plans</h3>
-        <p className='text-gray-800 text-sm'>With our wide Menu and Affordable prices, you can personalize your order.</p>
+        <p className='text-gray-800 text-md'>With our wide Menu and Affordable prices, you can personalize your order.</p>
       </div>
       <div className='flex m-14 sm:m-28 justify-center items-center flex-col'>
       <RiEBike2Fill  className="text-red-500  w-[100px] h-[100px]" />
         {/* <img src="images/whychoseus2.webp" className='w-[150] h-[100px]' alt="img2" /> */}
         <h3 className='font-bold text-xl mb-3'>Free and on time delivery</h3>
-        <p className='text-gray-800  text-sm'>Delivery services right to your door, free of charge. Pick the time and location for your food delivery.</p>
+        <p className='text-gray-800  text-md'>Delivery services right to your door, free of charge. Pick the time and location for your food delivery.</p>
 
       </div>
       <div className='flex m-14 sm:m-28 items-center flex-col'>
         {/* <img src="images/whychoseus3.jpg"  className='w-[150] h-[100px]' alt="" /> */}
         <MdHealthAndSafety  className="text-red-500  w-[100px] h-[100px]" />
         <h3 className='font-bold text-xl mb-3'>Safe and hygienic packaging</h3>
-        <p className='text-gray-800 text-sm'>
+        <p className='text-gray-800 text-md'>
 Every food item comes with careful packaging to preserve its deliciousness.</p>
 
       </div>
