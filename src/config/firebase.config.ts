3d1ea2172150b9cafc993a344647e681b1aacf63
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-02-10 13:09:35 +0530

    firebase keys added

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || "{}");
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC05DkjHlKwDMO3cDNo3JgvYifoesCPocs",
+    authDomain: "ie-mezz.firebaseapp.com",
+    projectId: "ie-mezz",
+    storageBucket: "ie-mezz.appspot.com",
+    messagingSenderId: "1013867000868",
+    appId: "1:1013867000868:web:2ac81517c7f17bf103d4b1",
+    measurementId: "G-R5CP4SWCQE"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
